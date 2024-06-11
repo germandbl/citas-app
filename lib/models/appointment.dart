dea31c7a34d12ae4f2d2bfd194b000f3b05ce2bf
germandbl <gdb.lopez@outlook.com>
2024-06-10 19:43:56 -0600

    Se agrega campo hospital

--- lib/models/appointment.dart
+++ lib/models/appointment.dart
@@ -1,10 +1,11 @@
 class Appointment {
   final String? title;
   final String? description;
+  final String? hospital;
   final String? schedule;
   final String? doctor;
 
-  Appointment(
+  Appointment(this.hospital,
       {required this.title,
       required this.description,
       required this.schedule,
