2f1d34ad90013d037cbeeaa1592b463deee92f7f
germandbl <gdb.lopez@outlook.com>
2024-06-10 22:01:49 -0600

    Campo de titulo cambiado a paciente

--- lib/models/appointment.dart
+++ lib/models/appointment.dart
@@ -1,12 +1,12 @@
 class Appointment {
-  final String? title;
+  final String? patient;
   final String? description;
   final String? hospital;
   final String? schedule;
   final String? doctor;
 
   Appointment(this.hospital,
-      {required this.title,
+      {required this.patient,
       required this.description,
       required this.schedule,
       required this.doctor});
