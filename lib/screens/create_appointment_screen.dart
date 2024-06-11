37df67c29d1bb784ad73047329e446e3d1142864
germandbl <gdb.lopez@outlook.com>
2024-06-10 21:39:09 -0600

    Pantalla para crear cita base

---
+++ lib/screens/create_appointment_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CreateAppointmentScreen extends StatelessWidget {
+  const CreateAppointmentScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
