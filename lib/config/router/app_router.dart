866121be9b84efc8e08884faf7155e3654612db7
germandbl <gdb.lopez@outlook.com>
2024-06-10 21:40:59 -0600

    Router y respectivas rutas nuevas agregadas

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,3 +1,5 @@
+import 'package:citas_app/screens/appointment_screen.dart';
+import 'package:citas_app/screens/create_appointment_screen.dart';
 import 'package:citas_app/screens/home_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
@@ -6,16 +8,22 @@ import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(routes: [
   GoRoute(
-    path: '/',
-    builder: (context, state) => StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if(!snapshot.hasData){
-            return const SignInScreen();
-          }
-          else {
-            return const HomeScreen();
-          }
-        }),
-  )
+      path: '/',
+      builder: (context, state) => StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const SignInScreen();
+            } else {
+              return const HomeScreen();
+            }
+          }),
+      routes: [
+        GoRoute(
+            path: 'create-appointment',
+            builder: (context, state) => const CreateAppointmentScreen()),
+        GoRoute(
+            path: 'appointment',
+            builder: (context, state) => const AppointmentScreen())
+      ])
 ]);
