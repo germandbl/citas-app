1275140ebf951c9cb4666b2409e1d31dee09e17e
germandbl <gdb.lopez@outlook.com>
2024-06-10 22:11:49 -0600

    Refactorizaciones

--- lib/models/appointment.dart
+++ lib/models/appointment.dart
@@ -1,12 +1,15 @@
 class Appointment {
+  final String? appointmentId;
   final String? patient;
   final String? description;
   final String? hospital;
   final String? schedule;
   final String? doctor;
 
-  Appointment(this.hospital,
-      {required this.patient,
+  Appointment(
+      {required this.appointmentId,
+      required this.hospital,
+      required this.patient,
       required this.description,
       required this.schedule,
       required this.doctor});
