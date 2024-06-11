0bff8f9ee70f0222fc543ed098335148ed885046
germandbl <gdb.lopez@outlook.com>
2024-06-10 21:20:35 -0600

    Nueva configuracion integrada
    Para implementar FirebaseUIAuth

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,91 +1,21 @@
-import 'package:citas_app/providers/app_state.dart';
 import 'package:citas_app/screens/home_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const HomeScreen(),
-      redirect: (context, state) {
-        final logged = context.read<ApplicationState>().loggedIn;
-    
-        if (logged == false) {
-          return '/sign-in';
-        } 
-        if(logged == true){
-          return '/';
-        }
-        return null;
-      },
-    ),
-    GoRoute(
-      path: '/sign-in',
-      builder: (context, state) {
-        return SignInScreen(
-          actions: [
-            ForgotPasswordAction(((context, email) {
-              final uri = Uri(
-                path: '/sign-in/forgot-password',
-                queryParameters: <String, String?>{
-                  'email': email,
-                },
-              );
-              context.push(uri.toString());
-            })),
-            AuthStateChangeAction(((context, state) {
-              final user = switch (state) {
-                SignedIn state => state.user,
-                UserCreated state => state.credential.user,
-                _ => null
-              };
-              if (user == null) {
-                return;
-              }
-              if (state is UserCreated) {
-                user.updateDisplayName(user.email!.split('@')[0]);
-              }
-              if (!user.emailVerified) {
-                user.sendEmailVerification();
-                const snackBar = SnackBar(
-                    content: Text(
-                        'Por favor mira tu correo electrónico para verificarlo'));
-                ScaffoldMessenger.of(context).showSnackBar(snackBar);
-              }
-              context.pushReplacement('/');
-            })),
-          ],
-        );
-      },
-      routes: [
-        GoRoute(
-          path: 'forgot-password',
-          builder: (context, state) {
-            final arguments = state.uri.queryParameters;
-            return ForgotPasswordScreen(
-              email: arguments['email'],
-              headerMaxExtent: 200,
-            );
-          },
-        ),
-      ],
-    ),
-    GoRoute(
-      path: '/profile',
-      builder: (context, state) {
-        return ProfileScreen(
-          providers: const [],
-          actions: [
-            SignedOutAction((context) {
-              context.pushReplacement('/');
-            }),
-          ],
-        );
-      },
-    ),
-  ],
-);
+final appRouter = GoRouter(routes: [
+  GoRoute(
+    path: '/',
+    builder: (context, state) => StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if(!snapshot.hasData){
+            return const SignInScreen();
+          }
+          else {
+            return const HomeScreen();
+          }
+        }),
+  )
+]);
