b9da1d260f960478fc123ff6c4645e92645a7841
germandbl <gdb.lopez@outlook.com>
2024-06-10 19:43:02 -0600

    Modelo de cita creado

---
+++ lib/models/appointment.dart
@@ -0,0 +1,12 @@
+class Appointment {
+  final String? title;
+  final String? description;
+  final String? schedule;
+  final String? doctor;
+
+  Appointment(
+      {required this.title,
+      required this.description,
+      required this.schedule,
+      required this.doctor});
+}
